a37f31832c184468ce99304b94257aa02b2886b7
adam.zitnansky@gmail.com <xzitna02@stud.fit.vutbr.cz>
2019-10-14 16:29:26 +0200

    add errror.h and fsm diagram base

---
+++ src/error.h
@@ -0,0 +1,17 @@
+#ifndef ERROR_H
+#define ERROR_H
+
+//macro aliases for error codes 
+
+#define LEXICAL_ERROR 1
+#define SYNTAX_ERROR 2
+#define UNDEFINE_REDEFINE_ERROR 3
+#define TYPE_CONVERSION_ERROR 4
+#define PARAM_COUNT_ERROR 5
+#define OTHER_SEMANTIC_ERROR 6
+#define ZERO_DIV_ERROR 9
+#define INTERNAL_ERROR 99
+
+
+#endif
+
