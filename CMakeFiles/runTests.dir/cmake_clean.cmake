c4d66d70c6102fac71422aac6b743dacf453413c
samuel <samuel.dudik@gmail.com>
2019-10-17 10:28:37 +0200

    Add cmake.gitignore, update CMakeLists.txt

---
+++ CMakeFiles/runTests.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/runTests.dir/src/tests/tests.cpp.o"
+  "bin/runTests"
+  "bin/runTests.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/runTests.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- src/tests/CMakeFiles/GData.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for GData.
-# This may be replaced when dependencies are built.
