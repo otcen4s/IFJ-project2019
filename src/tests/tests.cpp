db21e3976c03eccb44315061d4961e2bd4180b34
otcen4s <mato.otcenas@gmail.com>
2019-10-17 03:29:39 +0200

    Indentation fix

--- src/tests/tests.cpp
+++ src/tests/tests.cpp
@@ -4,17 +4,17 @@
 tString string;
 
 TEST(string_lib, str_copy){
-    
     str_copy(&string, "ahoj");
     EXPECT_STREQ(string.str,"ahoj");
     EXPECT_EQ(string.len, 5);
-   
 }
+
 TEST(string_lib, str_copy_arr_size){
     str_realloc(&string, 10);
     EXPECT_EQ(string.arr_size,128+10);
     EXPECT_NE(string.arr_size, 139);
 }
+
 TEST(string_lib, str_cmp_keyword){
     str_copy(&string, "for");
     EXPECT_TRUE(str_cmp_keyword(&string, "for"));
