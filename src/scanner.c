b31cb162739d5474346d7dcf76939ba986427909
Adam Zitnansky <xzitna02@fit.vutbr.cz>
2019-10-27 17:34:34 +0100

    added states into scanner fsm

--- src/scanner.c
+++ src/scanner.c
@@ -4,6 +4,7 @@
 Scanner scan;
 Scanner *scanner = &scan;
 
+
 int init_scanner(Scanner *s, const char* file_name)
 { 
     if (s->src_file=fopen(file_name, "r")) return INTERNAL_ERROR;
@@ -88,13 +89,17 @@ Token create_string_token(tString string, int *error) {
 
 Token read_token(int *err)
 {
-    Token  token;
+    Token  empty_token;
+    *err = NO_ERROR;
+    
+    //used to store hexadecimal sequence for conversion
+    char hexa_number_str[3] = {0};
+
     //init atribute string
     if(str_init(scanner->atr_string))
     {
-        return token;
+        return empty_token ; //returning empty token in case of error
         *err = INTERNAL_ERROR;
-
     } 
 
 
@@ -102,48 +107,235 @@ Token read_token(int *err)
     {
        //read character form file
        scanner->curr_char=getc(scanner->src_file);
-       Token  token;
-  
-
+       Token token;
 
         switch (scanner->state)
         {
-        case STATE_START: 
-            if (scanner->is_line_start) scanner->state = STATE_INDENTATION_CHECK;   // indentation check
-            else if((scanner->curr_char == ' ') && (!scanner->is_line_start)) scanner->state = STATE_START; //ignore space if its not indent    
-            else if(scanner->curr_char == '+') scanner->state = STATE_PLUS;
-            else if (scanner->curr_char == '*') scanner->state = STATE_MULTIPLICATION;
-            else if (scanner->curr_char == '-') scanner->state = STATE_MINUS;  
-            else if (scanner->curr_char == '/') scanner->state = STATE_DIVISON;    
-            else if (scanner->curr_char == '>') scanner->state = STATE_GREATER_THAN;
-            else if (scanner->curr_char == '<') scanner->state = STATE_RIGHT_BRACKET;
-            else if (scanner->curr_char == '(') scanner->state = STATE_LEFT_BRACKET;            
-            else if (scanner->curr_char == ')') scanner->state = STATE_RIGHT_BRACKET;
-            else if (scanner->curr_char == '=') scanner->state = STATE_ASSIGN;
-            else if (scanner->curr_char == EOF) scanner->state = STATE_EOF;
-            else if (scanner->curr_char == '\n' ) scanner->state = STATE_EOL;
-            else if (scanner->curr_char == ',') scanner->state = STATE_COMMA;
-            else if (scanner->curr_char == '#') scanner->state = STATE_HASH;
-            else if (isalpha(scanner->curr_char) || scanner->curr_char == '_' ) scanner->state = STATE_ID;
-            else if (scanner->curr_char == '#') scanner->state = STATE_HASH;
-            else if (scanner->curr_char == '"') scanner->state = STATE_STRING_START;
-            else scanner->state = STATE_ERROR; //
+            case STATE_START: 
+                if (scanner->is_line_start) scanner->state = STATE_INDENTATION_CHECK;   // indentation check
+                else if((scanner->curr_char == ' ') && (!scanner->is_line_start)) scanner->state = STATE_START; //ignore space if its not indent    
+                else if(scanner->curr_char == '+') scanner->state = STATE_PLUS;
+                else if (scanner->curr_char == '*') scanner->state = STATE_MULTIPLICATION;
+                else if (scanner->curr_char == '-') scanner->state = STATE_MINUS;  
+                else if (scanner->curr_char == '/') scanner->state = STATE_DIVISON;    
+                else if (scanner->curr_char == '>') scanner->state = STATE_GREATER_THAN;
+                else if (scanner->curr_char == '<') scanner->state = STATE_RIGHT_BRACKET;
+                else if (scanner->curr_char == '(') scanner->state = STATE_LEFT_BRACKET;            
+                else if (scanner->curr_char == ')') scanner->state = STATE_RIGHT_BRACKET;
+                else if (scanner->curr_char == '=') scanner->state = STATE_ASSIGN;
+                else if (scanner->curr_char == EOF) scanner->state = STATE_EOF;
+                else if (scanner->curr_char == '\n' ) scanner->state = STATE_EOL;
+                else if (scanner->curr_char == ',') scanner->state = STATE_COMMA;
+                else if (scanner->curr_char == '#') scanner->state = STATE_COMMENT_SINGLE_LINE;
+                else if (isalpha(scanner->curr_char) || scanner->curr_char == '_' ) scanner->state = STATE_ID;
+                else if (scanner->curr_char == '\'') scanner->state = STATE_STRING_START;
+                else scanner->state = STATE_ERROR;      
+                break;
             
-
-        break;
-        
-        case STATE_PLUS:
-            token.type=TOKEN_PLUS;
-        
-        
-
-        
+            case STATE_ERROR:
+                *err = LEXICAL_ERROR;
+                return token; 
+                break;
             
+            case STATE_PLUS:
+                token.type=TOKEN_PLUS;
+                ungetc(scanner->curr_char, scanner->src_file);
+                return token;
+                break;
+            
+            case STATE_MINUS:
+                token.type= TOKEN_MINUS;
+                ungetc(scanner->curr_char, scanner->src_file);
+                return token; 
+                break;
+            
+            case STATE_MULTIPLICATION:
+                token.type= TOKEN_MULTIPLICATION;
+                ungetc(scanner->curr_char, scanner->src_file);
+                return token; 
+                break;
+            
+            case STATE_DIVISON:
+                token.type= TOKEN_DIVISION;
+                ungetc(scanner->curr_char, scanner->src_file);
+                return token; 
+                break;
 
-        break;
+            case STATE_GREATER_THAN:
+                if(scanner->curr_char == '=')
+                {
+                    scanner->state = STATE_GREATER_EQUAL;
+                }
+                else
+                {
+                token.type= TOKEN_GREATER_THAN;
+                ungetc(scanner->curr_char, scanner->src_file);
+                return token;
+                }
+                break;
+            
+            case STATE_LESSER_THAN:
+                if(scanner->curr_char == '=')
+                {
+                    scanner->state = STATE_LESSER_EQUAL;
+                }
+                else
+                {
+                token.type= TOKEN_LESSER_THAN;
+                ungetc(scanner->curr_char, scanner->src_file);
+                return token;
+                }
+                break;
+            
+            case STATE_GREATER_EQUAL:
+                token.type= TOKEN_GREATER_EQUAL;
+                ungetc(scanner->curr_char, scanner->src_file);
+                return token; 
+                break;
+            
+            case STATE_LESSER_EQUAL:
+                token.type= TOKEN_LESSER_EQUAL;
+                ungetc(scanner->curr_char, scanner->src_file);
+                return token; 
+                break;
+
+            case STATE_EOL:
+                token.type= TOKEN_EOL;
+                ungetc(scanner->curr_char, scanner->src_file);
+                return token; 
+                break;
+
+            case STATE_EOF:
+                token.type= TOKEN_EOF;
+                ungetc(scanner->curr_char, scanner->src_file);
+                return token; 
+                break;
+
+            case STATE_LEFT_BRACKET:
+                token.type= TOKEN_LEFT_BRACKET;
+                ungetc(scanner->curr_char, scanner->src_file);
+                return token; 
+                break;
+            
+            case STATE_RIGHT_BRACKET:
+                token.type= TOKEN_RIGHT_BRACKET;
+                ungetc(scanner->curr_char, scanner->src_file);
+                return token; 
+                break;
+
+            case STATE_COMMA:
+                token.type= TOKEN_COMMA;
+                ungetc(scanner->curr_char, scanner->src_file);
+                return token; 
+                break;
+
+            case STATE_COMMENT_SINGLE_LINE:
+                if(scanner->curr_char == '\n' || scanner->curr_char  == EOF)
+                {
+                    ungetc(scanner->curr_char, scanner->src_file);
+                    scanner->state = STATE_START;
+                }
+                break;
+
+            case STATE_ASSIGN:
+                if(scanner->curr_char == '=')
+                {
+                    scanner->state = STATE_EQUAL;
+                } 
+                else
+                {
+                    ungetc(scanner->curr_char, scanner->src_file);
+                    token.type = TOKEN_ASSIGN;
+                    return token; 
+                }
+                break;
+            
+            case STATE_EQUAL:
+                token.type = TOKEN_EQUAL;
+                return token;
+                break;
+            
+                    
+            case STATE_STRING_START:
+                if(scanner->curr_char == '\'')
+                {
+                    scanner->state = STATE_STRING_END;
+                }
+                else if (scanner->curr_char == '\\')                {
+                    scanner->state = STATE_STRING_ESCAPE_SEQ;
+                }
+                else if (scanner->curr_char < 32) //invalid character
+                {
+                    scanner->state = STATE_ERROR;
+                    *err = INTERNAL_ERROR;
+                }
+                else
+                {
+                    str_insert_char(scanner->atr_string,scanner->curr_char); //TODO fail check
+                }
+                break;
+
+            case STATE_STRING_ESCAPE_SEQ:
+                if(scanner->curr_char == 'n')
+                {
+                    str_insert_char(scanner->atr_string,'\n'); //TODO fail check
+                }
+                else if (scanner->curr_char == 't')
+                {
+                    str_insert_char(scanner->atr_string,'\t'); //TODO fail check
+                }
+                else if (scanner->curr_char == '\\')
+                {
+                    str_insert_char(scanner->atr_string,'\\'); //TODO fail check
+                }
+                else if (scanner->curr_char == '\'')
+                {
+                    str_insert_char(scanner->atr_string,'\''); //TODO fail check
+                }
+                // check for hexa decimal escape sequence
+                else if (
+                        (tolower(scanner->curr_char) >= 'a' && tolower(scanner->curr_char) <= 'f') ||
+                        (scanner->curr_char >= '0' && scanner->curr_char <= '9')
+                        )
+                {
+                    scanner->state = STATE_STR_ESCAPE_HEXA;
+                    hexa_number_str[0] = scanner->curr_char;
+                }
+                //invalid escape sequence will be inserted as 2 separate chars
+                else 
+                {
+                    str_insert_char(scanner->atr_string,'\\'); //TODO fail check
+                    str_insert_char(scanner->atr_string,scanner->curr_char);
+                }  
+                break;
+
+            case STATE_STR_ESCAPE_HEXA:
+                if (
+                        (tolower(scanner->curr_char) >= 'a' && tolower(scanner->curr_char) <= 'f') ||
+                        (scanner->curr_char >= '0' && scanner->curr_char <= '9')
+                    )
+                {
+                    hexa_number_str[1]=scanner->curr_char; 
+                    char* endptr;
+                    char result= (char) strtol(hexa_number_str, NULL, 16);  //TODO fail check
+                    str_insert_char(scanner->atr_string,result);
+                }
+                else
+                // not valid hexa decimal number, will be writen into resulting string as if there was no escape sequence i.e. one by one
+                { 
+                    str_insert_char(scanner->atr_string,'\\');
+                    str_insert_char(scanner->atr_string,hexa_number_str[0]); //TODO fail check
+                    str_insert_char(scanner->atr_string,scanner->curr_char);
+                }          
+            break;
+            
+            //case STATE_STRING_END:
+        }       
+            
+    
         
 
-        }
+            
     }
 
 
