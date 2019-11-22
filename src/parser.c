db9181b8a7d7110b4575e06ef0567616cb8e0177
Adam Zitnansky <xzitna02@fit.vutbr.cz>
2019-11-22 09:35:41 +0100

    repalced temporary return in compiler_start function with statement func call

--- src/parser.c
+++ src/parser.c
@@ -135,9 +135,7 @@ int start_compiler(char* src_file_name)
     if(err) return err;
 
     //start syntactic analysis here e.i. call function which implements <program> rule 
-
-    //temporary
-    return NO_ERROR;    
+    return statement(parser);
 }
 
 
