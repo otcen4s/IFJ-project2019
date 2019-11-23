f01666f5af2d82e1c4d609cfcf97e0a40e1ec458
Lívia Žitňanská <49245534+liviazitnanska@users.noreply.github.com>
2019-11-23 19:19:31 +0100

    Edit of tests becouse symtable was edited as well

--- test/symtable_test.c
+++ test/symtable_test.c
@@ -3,7 +3,7 @@
 
 void setUp(void)
 {
-    tSymbol table;
+    tSymbol *table;
     symtab_init(&table);
 }
 
@@ -14,7 +14,7 @@ void tearDown(void)
 void test_symtab_init(void)
 {
     tSymbol* table = NULL;
-    symtab_init(table);
+    symtab_init(&table);
 
     TEST_ASSERT_TRUE(table->size == 0);
     TEST_ASSERT_TRUE(table->arr_size == SYMTAB_SIZE);
@@ -27,44 +27,38 @@ void test_symtab_init(void)
 void test_symtab_add(void)
 {
     tSymbol* table = NULL;
-    symtab_init(table);
-    Symbol_value one;
-    one.int_val = 1;
-    one.double_val = -2.4;
-    int help=2;
+    symtab_init(&table);
+    tSymbol_item *help;
 
-    help = symtab_add(NULL,"1", SYMBOL_VAR, DATA_INT,one,SYMBOL_DECLARED); //prazdna tabulka
-    TEST_ASSERT_TRUE(help==-1);
-    printf("HELLO");
+    help = symtab_add(NULL,"1",0); //prazdna tabulka
+    TEST_ASSERT_TRUE(strcmp(help->key,"1")==0);
+    printf("!!!!!!!!!!!!!!!HELLO!!!!!!!!!!!!!!!!!!!!!!!!!!!!!§");
     //otestovanie INT hodnoty
-    help = symtab_add(table,"jedna",SYMBOL_VAR, DATA_INT, one, SYMBOL_DECLARED); //pridam jednotku
-    TEST_ASSERT_TRUE(help==0);
+    help = symtab_add(table,"jedna",0); //pridam jednotku
+    TEST_ASSERT_TRUE(strcmp(help->key,"jedna")==0);
     TEST_ASSERT_TRUE(table->size==1);
-    symtab_add(table,"dva",SYMBOL_VAR, DATA_INT, one, SYMBOL_DECLARED); //pridam jednotku
-    symtab_add(table,"tri",SYMBOL_VAR, DATA_INT, one, SYMBOL_DECLARED); //pridam jednotku
+    symtab_add(table,"dva",0); //pridam jednotku
+    symtab_add(table,"tri",0); //pridam jednotku
     TEST_ASSERT_TRUE(table->size==3);
 
     //Otestovanie double
-    help = symtab_add(table,"minus_dva",SYMBOL_VAR,DATA_DOUBLE,one,SYMBOL_DEFINED); //mala by tam byt -2.4
-    TEST_ASSERT_TRUE(help==0);
+    help = symtab_add(table,"minus_dva",0); //mala by tam byt -2.4
+    TEST_ASSERT_TRUE(help->key==0);
     TEST_ASSERT_TRUE(table->size==4);
 
     //HLADANIE
-    tSymbol* search = NULL;
-    search = symtab_lookup(table,"jedna");
+    tSymbol_item* search = NULL;
+    search = symtab_lookup(table,"jedna",0);
     TEST_ASSERT_NOT_NULL(search);
-    search = symtab_lookup(table,"minus_dva");
+    search = symtab_lookup(table,"minus_dva",0);
     TEST_ASSERT_NOT_NULL(search);
 }
 
 void test_symtab_delete(void)
 {
     tSymbol* table = NULL;
-    Symbol_value one;
-    one.int_val = 1;
-
-    symtab_init(table); //inicializacia tabulky
-    symtab_add(table,"minus_dva",SYMBOL_VAR,DATA_INT,one,SYMBOL_DEFINED); //pridanie prvku
+    symtab_init(&table); //inicializacia tabulky
+    symtab_add(table,"minus_dva",0); //pridanie prvku
     symtab_clear(table);
     TEST_ASSERT_TRUE(table->size==0);
     TEST_ASSERT_TRUE(table->item_array[1]==NULL);
